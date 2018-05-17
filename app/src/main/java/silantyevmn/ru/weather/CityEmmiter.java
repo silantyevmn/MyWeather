dc4a86191da31a74a58d8c1676990665777f6d65
silantyevmn <5992033@mail.ru>
2018-05-17 15:38:39 +0300

    new class CityEmmiter

--- app/src/main/java/silantyevmn/ru/weather/CityEmmiter.java
+++ app/src/main/java/silantyevmn/ru/weather/CityEmmiter.java
@@ -1,8 +1,32 @@
 package silantyevmn.ru.weather;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 /**
  * Created by silan on 17.05.2018.
  */
 
 public class CityEmmiter {
+    private static ArrayList<City> cities;
+
+    public static ArrayList<City> getCities() {
+        return cities;
+    }
+
+    public static void initNewCityParam(Context context,int minTemperature,int maxTemperature){
+        String[] arrCity=context.getResources().getStringArray(R.array.city_selection);
+        cities=new ArrayList<>();
+        for (int i = 0; i < arrCity.length; i++) {
+            String name=arrCity[i].toString();
+            int temperature=getRandomMinMax(minTemperature,maxTemperature);
+            cities.add(i,new City(name,temperature));
+        }
+    }
+
+    private static int getRandomMinMax(int min,int max){
+        return (int)(Math.random()*(max-min))+min;
+    }
 }
