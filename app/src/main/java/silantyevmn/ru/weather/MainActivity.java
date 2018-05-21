02754cef454add350f2d1ac6cd754f3f322d6981
silantyevmn <5992033@mail.ru>
2018-05-21 12:30:04 +0300

    add metod save and load add init WeatherPreferencer add switch humidity,pressure and wind

--- app/src/main/java/silantyevmn/ru/weather/MainActivity.java
+++ app/src/main/java/silantyevmn/ru/weather/MainActivity.java
@@ -1,18 +1,19 @@
 package silantyevmn.ru.weather;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private WeatherPreferencer weatherPreferencer;
     private TextView textViewDescription;
     private EditText editTextCity;
+    private Switch humiditySwitch, pressureSwitch, windSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,41 +23,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherPreferencer = new WeatherPreferencer(MainActivity.this, savedInstanceState);
         // Объявляем и находим ресурсы
         Button button = findViewById(R.id.button_show_description);
         textViewDescription = findViewById(R.id.textview_description);
         editTextCity = findViewById(R.id.edit_text_city);
-        editTextCity.requestFocus();
+        humiditySwitch = findViewById(R.id.switch_humidity);
+        pressureSwitch = findViewById(R.id.switch_pressure);
+        windSwitch = findViewById(R.id.switch_wind);
         // Устанавливаем слушатель нажатий
         button.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.button_show_description) {
-
-            int position = CityEmmiter.getPositionFindCity(editTextCity.getText().toString());
-            if(position==-1){
-                textViewDescription.setText("Город не найден");
-                return;
-            }
-            String temperature = CityEmmiter.getCities().get(position).getTemperature();
-            textViewDescription.setText(temperature);
+        //если первый запуск,считаваем показатели чекбоксов из памяти
+        if (savedInstanceState == null) {
+            load();
         }
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
+    public void onClick(View view) {
+        int position = 0;
+        if (view.getId() == R.id.button_show_description) {
+            position = CityEmmiter.getPositionFindCity(editTextCity.getText().toString());
+            if (position == -1) {
+                textViewDescription.setText(R.string.text_city_not_found);
+                return;
+            }
+            showWeatherNewActivity(position);
+        }
+    }
+
+    private void showWeatherNewActivity(int position) {
+        Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+        //передаем позицию ид в новую активити
+        intent.putExtra(WeatherActivity.EXTRA_POSITION_ID, position);
+        //записываем показатели чекбоксов
+        CityEmmiter.setIsHumidity(humiditySwitch.isChecked());
+        CityEmmiter.setIsPressure(pressureSwitch.isChecked());
+        CityEmmiter.setIsWind(windSwitch.isChecked());
+        //запускаем активити
+        startActivity(intent);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        save();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    private void load() {
+        //считываем показатели
+        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null) {
+            humiditySwitch.setChecked(weatherPreferencer.getIsHumidity());
+            pressureSwitch.setChecked(weatherPreferencer.getIsPressure());
+            windSwitch.setChecked(weatherPreferencer.getIsWind());
+        }
     }
+
+    private void save() {
+        //записываем показатели
+        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null) {
+            weatherPreferencer.save(humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
+        }
+    }
+
 }
