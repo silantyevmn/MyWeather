8583da8d0e1f445827f93a350ce0a972022fcd4d
silantyevmn <5992033@mail.ru>
2018-05-24 13:43:32 +0300

    update metod showScreenValue optimize code

--- app/src/main/java/silantyevmn/ru/weather/WeatherActivity.java
+++ app/src/main/java/silantyevmn/ru/weather/WeatherActivity.java
@@ -3,13 +3,13 @@ package silantyevmn.ru.weather;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION_ID = "position_id";
     private final String SEPARATOR = ": ";
     private City city;
-    private WeatherPreferencer weatherPreferencer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,27 +19,23 @@ public class WeatherActivity extends AppCompatActivity {
         int position = getIntent().getIntExtra(EXTRA_POSITION_ID, 0);
         //иницилизируем класс Сити
         city = CityEmmiter.getCities().get(position);
-        //находим текстовые поля
+        //находим текстовые поля устанавливаем и выводим значения
         TextView textViewCityName = findViewById(R.id.text_view_city);
-        TextView textViewTemperature = findViewById(R.id.text_view_temperature);
-        TextView textViewHumidity = findViewById(R.id.text_view_humidity);
-        TextView textViewPressure = findViewById(R.id.text_view_pressure);
-        TextView textViewWind = findViewById(R.id.text_view_wind);
-        //устанавливаем и выводим значения
-        showTextView(textViewCityName, city.getName());
-        showTextView(textViewTemperature, city.getTemperature(getString(R.string.text_prefix_temperature)));
+        textViewCityName.setText(textViewCityName.getText().toString() + SEPARATOR + city.getName());
+        showScreenValue(R.id.layout_temperature, R.id.text_view_temperature, city.getTemperature(getString(R.string.text_prefix_temperature)));
         if (CityEmmiter.getIsHumidity())
-            showTextView(textViewHumidity, city.getHumidity(getString(R.string.text_prefix_humidity)));
+            showScreenValue(R.id.layout_humidity, R.id.text_view_humidity, city.getHumidity(getString(R.string.text_prefix_humidity)));
         if (CityEmmiter.getIsPressure())
-            showTextView(textViewPressure, city.getPressure(getString(R.string.text_prefix_pressure)));
+            showScreenValue(R.id.layout_pressure, R.id.text_view_pressure, city.getPressure(getString(R.string.text_prefix_pressure)));
         if (CityEmmiter.getIsWind())
-            showTextView(textViewWind, city.getWind(getString(R.string.text_prefix_wind)));
+            showScreenValue(R.id.layout_wind, R.id.text_view_wind, city.getWind(getString(R.string.text_prefix_wind)));
 
     }
 
-    private void showTextView(TextView v, String value) {
-        String text = v.getText().toString();
-        v.setVisibility(View.VISIBLE);
-        v.setText(text + SEPARATOR + value);
+    private void showScreenValue(int idLayout, int idTextView, String value) {
+        LinearLayout linearLayout = findViewById(idLayout);
+        linearLayout.setVisibility(View.VISIBLE);
+        TextView textView = findViewById(idTextView);
+        textView.setText(value);
     }
 }
