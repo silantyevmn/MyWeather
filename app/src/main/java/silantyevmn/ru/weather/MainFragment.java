337f69c5dc61034e0e0cc5835068512de2db4c0c
silantyevmn <5992033@mail.ru>
2018-05-30 14:02:30 +0300

    changed the code, optimization, added variables isHumidity,currentCity,isPressure,isWind

--- app/src/main/java/silantyevmn/ru/weather/MainFragment.java
+++ app/src/main/java/silantyevmn/ru/weather/MainFragment.java
@@ -3,6 +3,8 @@ package silantyevmn.ru.weather;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,21 +25,22 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     private onSelectedButtonListener listener;
 
     public interface onSelectedButtonListener {
-        public void onSelectedButton(int position);
-
-        public void onSelectedSwitch(boolean humiditySwitch, boolean pressureSwitch, boolean windSwitch);
+        public void onClickButton(String city, boolean isHumidity, boolean isPressure, boolean isWind);
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        //setRetainInstance(true);
         listener = (onSelectedButtonListener) getActivity();
-        weatherPreferencer = new WeatherPreferencer(getActivity(), savedInstanceState);
+        weatherPreferencer = WeatherPreferencer.getInstance(getActivity());
         // Объявляем и находим ресурсы
-        Button button = rootView.findViewById(R.id.button_show_description);
-        textViewDescription = rootView.findViewById(R.id.textview_description);
         editTextCity = rootView.findViewById(R.id.edit_text_city);
+        final Button button = rootView.findViewById(R.id.button_show_description);
+        button.setEnabled(!editTextCity.getText().toString().trim().isEmpty());
+        textViewDescription = rootView.findViewById(R.id.textview_description);
         humiditySwitch = rootView.findViewById(R.id.switch_humidity);
         pressureSwitch = rootView.findViewById(R.id.switch_pressure);
         windSwitch = rootView.findViewById(R.id.switch_wind);
@@ -46,7 +49,24 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         humiditySwitch.setOnClickListener(this);
         pressureSwitch.setOnClickListener(this);
         windSwitch.setOnClickListener(this);
+        editTextCity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                button.setEnabled(!editable.toString().trim().isEmpty());
+            }
+        });
         if (savedInstanceState == null) {
+            //считываем из памяти значения
             load();
         }
 
@@ -55,39 +75,55 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        int position = 0;
+        String currentCity = editTextCity.getText().toString().trim();
         if (view.getId() == R.id.button_show_description) {
-            position = CityEmmiter.getPositionFindCity(editTextCity.getText().toString());
-            if (position == -1) {
+            //проверка есть город в базе?
+            if (CityEmmiter.getPositionFindCity(currentCity) == -1) {
                 textViewDescription.setText(R.string.text_city_not_found);
-                return;
+                //проверка на 2 фрагмент
+                if (getActivity().findViewById(R.id.fragment_weather) == null) {
+                    //не запускаем 2 активити,если не найден город
+                    return;
+                }
             }
-            listener.onSelectedButton(position);
+            listener.onClickButton(currentCity, humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
         }
         if (view.getId() == R.id.switch_pressure || view.getId() == R.id.switch_humidity || view.getId() == R.id.switch_wind) {
-            listener.onSelectedSwitch(humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
+            listener.onClickButton(currentCity, humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
             save();
         }
     }
 
     private void load() {
         //считываем показатели
-        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null) {
+        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null && editTextCity != null) {
+            editTextCity.setText(weatherPreferencer.getEditTextCity());
             humiditySwitch.setChecked(weatherPreferencer.getIsHumidity());
             pressureSwitch.setChecked(weatherPreferencer.getIsPressure());
             windSwitch.setChecked(weatherPreferencer.getIsWind());
-            //временно не успеваю, позже переделаю
-            CityEmmiter.setIsHumidity(weatherPreferencer.getIsHumidity());
-            CityEmmiter.setIsPressure(weatherPreferencer.getIsPressure());
-            CityEmmiter.setIsWind(weatherPreferencer.getIsWind());
         }
     }
 
     private void save() {
         //записываем показатели
-        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null) {
-            weatherPreferencer.save(humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
+        if (humiditySwitch != null && pressureSwitch != null && windSwitch != null && editTextCity != null) {
+            weatherPreferencer.save(editTextCity.getText().toString().trim(), humiditySwitch.isChecked(), pressureSwitch.isChecked(), windSwitch.isChecked());
         }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //записываем значение города при перевороте экрана в Bundle
+        outState.putString(WeatherPreferencer.KEY_CURRENT_CITY, editTextCity.getText().toString().trim());
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        //считываем значения города при перевороте экрана
+        if (savedInstanceState != null) {
+            editTextCity.setText(savedInstanceState.getString(WeatherPreferencer.KEY_CURRENT_CITY, WeatherPreferencer.CURRENT_CITY_DEFAULT));
+        }
+    }
 }
