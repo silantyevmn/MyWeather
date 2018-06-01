ff63167e4f6acec14c7e5b7c602ccb51db7b4e94
silantyevmn <5992033@mail.ru>
2018-06-01 18:59:49 +0300

    optimize cods

--- app/src/main/java/silantyevmn/ru/weather/MainActivity.java
+++ app/src/main/java/silantyevmn/ru/weather/MainActivity.java
@@ -15,22 +15,21 @@ public class MainActivity extends AppCompatActivity implements MainFragment.onSe
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // запускаем фрагменты и начинаем транзакцию
+        // если первый запуск
         if (savedInstanceState == null) {
-           /* MainFragment fragment1 = new MainFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_main, fragment1)
-                    .commit();*/
-            FrameLayout fragment2 = findViewById(R.id.fragment_weather);
-            WeatherPreferencer weatherPreferencer = WeatherPreferencer.getInstance(MainActivity.this);
+            FrameLayout fragment2 = findViewById(R.id.fragment_details);
+            //проверяем есть в активити фрагмент деталей погоды?
             if (fragment2 != null) {
-                WeatherFragment weatherFragment = WeatherFragment.newInstance(
-                        weatherPreferencer.getEditTextCity(),
-                        weatherPreferencer.getIsHumidity(),
-                        weatherPreferencer.getIsPressure(),
-                        weatherPreferencer.getIsWind());
+                //если есть, то показываем его
+                DetailsFragment detailsFragment = DetailsFragment.newInstance(
+                        //передаем во фрагмент значение из памяти приложения
+                        DataPreferences.getEditTextCity(this),
+                        DataPreferences.getIsHumidity(this),
+                        DataPreferences.getIsPressure(this),
+                        DataPreferences.getIsWind(this));
+                //запускаем транзакцию и добавляем фрагмент
                 getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_weather, weatherFragment)
+                        .add(R.id.fragment_details, detailsFragment)
                         .commit();
             }
         }
@@ -38,24 +37,25 @@ public class MainActivity extends AppCompatActivity implements MainFragment.onSe
 
     @Override
     public void onClickButton(String currentCity, boolean isHumidity, boolean isPressure, boolean isWind) {
+        int position=CityEmmiter.getPositionFindCity(currentCity);
         // Получаем ссылку на второй фрагмент по ID
-        FrameLayout fragment = findViewById(R.id.fragment_weather);
+        FrameLayout fragment = findViewById(R.id.fragment_details);
         // если фрагмента не существует
         if (fragment == null) {
             // запускаем активность, если город нашелся
-            if (CityEmmiter.getPositionFindCity(currentCity) != -1) {
-                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
-                intent.putExtra(WeatherPreferencer.KEY_CURRENT_CITY, currentCity);
-                intent.putExtra(WeatherPreferencer.KEY_HUMIDITY, isHumidity);
-                intent.putExtra(WeatherPreferencer.KEY_PRESSURE, isPressure);
-                intent.putExtra(WeatherPreferencer.KEY_WIND, isWind);
+            if (position != -1) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DataPreferences.KEY_CURRENT_CITY, currentCity);
+                intent.putExtra(DataPreferences.KEY_HUMIDITY, isHumidity);
+                intent.putExtra(DataPreferences.KEY_PRESSURE, isPressure);
+                intent.putExtra(DataPreferences.KEY_WIND, isWind);
                 startActivity(intent);
             }
         } else {
             // Выводим 2-фрагмент
-            WeatherFragment weatherFragment = WeatherFragment.newInstance(currentCity, isHumidity, isPressure, isWind);
+            DetailsFragment detailsFragment = DetailsFragment.newInstance(currentCity, isHumidity, isPressure, isWind);
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_weather, weatherFragment)
+                    .replace(R.id.fragment_details, detailsFragment)
                     .commit();
         }
     }
