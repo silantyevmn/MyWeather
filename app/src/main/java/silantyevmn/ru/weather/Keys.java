f5fc0e7ca710476503555335e287bc2068d072b0
silantyevmn <5992033@mail.ru>
2018-06-03 15:07:41 +0300

    delete metod getCities

--- app/src/main/java/silantyevmn/ru/weather/Keys.java
+++ app/src/main/java/silantyevmn/ru/weather/Keys.java
@@ -2,7 +2,9 @@ package silantyevmn.ru.weather;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
@@ -16,8 +18,8 @@ public class Keys {
     public static final String KEY_HUMIDITY = "key_humidity";
     public static final String KEY_PRESSURE = "key_pressure";
     public static final String KEY_WIND = "key_wind";
-    public static final String KEY_POSITION="position";
-    public static final int POSITION_DEFAULT=0;
+    public static final String KEY_POSITION = "position";
+    public static final int POSITION_DEFAULT = 0;
 
     public static int getPosition(Activity activity) {
         return activity.getPreferences(Context.MODE_PRIVATE).getInt(KEY_POSITION, POSITION_DEFAULT);
@@ -33,5 +35,10 @@ public class Keys {
 
     public static boolean getIsWind(Activity activity) {
         return activity.getPreferences(Context.MODE_PRIVATE).getBoolean(KEY_WIND, WIND_DEFAULT);
+    }
+
+    public static Set<String> getCities(MainActivity mainActivity) {
+        Set<String> cities = mainActivity.getPreferences(Context.MODE_PRIVATE).getStringSet("cities", new HashSet<String>());
+        return cities;
     }
 }
