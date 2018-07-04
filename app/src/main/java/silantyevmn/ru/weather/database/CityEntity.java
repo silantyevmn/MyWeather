c085da15ed20aba3f11c22c5dbea26477ea94fca
silantyevmn <5992033@mail.ru>
2018-07-04 17:46:09 +0300

    add fields

--- app/src/main/java/silantyevmn/ru/weather/database/CityEntity.java
+++ app/src/main/java/silantyevmn/ru/weather/database/CityEntity.java
@@ -16,6 +16,48 @@ public class CityEntity {
     @ColumnInfo( name = "name")
     private String name;
 
+    @ColumnInfo
+    private int temperature;
+
+    @ColumnInfo
+    private int humidity; //влажность
+
+    @ColumnInfo
+    private int pressure; //давление
+
+    @ColumnInfo
+    private int wind; //скорость ветра
+
+    @ColumnInfo
+    private String countryCode; //ru,en...
+
+    @ColumnInfo
+    private String icon; //01d,02n...
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setWind(int wind) {
+        this.wind = wind;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
     @NotNull
     public String getName() {
         return name;
@@ -27,5 +69,52 @@ public class CityEntity {
 
     public CityEntity(@NotNull String name) {
         this.name = name;
+    }
+
+    public String getTemperature(String prefix) {
+        if (temperature > 0) {
+            return "+" + temperature + prefix;
+        } else if (temperature < 0) {
+            return "-" + temperature + prefix;
+        } else return temperature + prefix;
+    }
+
+    public String getHumidity(String prefix) {
+        //String prefix=context.getResources().getString(R.string.text_prefix_humidity);
+        return humidity + prefix;
+    }
+
+    public String getPressure(String prefix) {
+        //String prefix=context.getResources().getString(R.string.text_prefix_pressure);
+        return pressure + prefix;
+    }
+
+    public String getWind(String prefix) {
+        //String prefix=context.getResources().getString(R.string.text_prefix_wind);
+        return wind + prefix;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public int getWind() {
+        return wind;
+    }
+
+    public String getIcon() {
+        return icon;
     }
 }
