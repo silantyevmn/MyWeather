0cbd9614a9307f1cc766cd51a53d49a5f7b50fb6
silantyevmn <5992033@mail.ru>
2018-07-09 12:43:20 +0300

    new class

---
+++ app/src/main/java/silantyevmn/ru/weather/utils/MyFirebaseInstance.java
@@ -0,0 +1,27 @@
+package silantyevmn.ru.weather.utils;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by silan on 09.07.2018.
+ */
+
+public class MyFirebaseInstance extends FirebaseInstanceIdService {
+    private final String TAG = "PushIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Получить ключ установки приложения на устройство
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    // Метод отправки ключа в вашу БД
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+
+}
