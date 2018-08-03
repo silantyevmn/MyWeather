f1478afef5cede73291611a7420150d5b37e04c1
silantyevmn <5992033@mail.ru>
2018-08-03 15:22:23 +0300

    add metod location

--- app/src/main/java/silantyevmn/ru/weather/utils/json/retrofit/IOpenWeather.java
+++ app/src/main/java/silantyevmn/ru/weather/utils/json/retrofit/IOpenWeather.java
@@ -12,7 +12,12 @@ public interface IOpenWeather {
     //@GET("data/2.5/weather")
     @GET("data/2.5/weather")
     Call<WeatherRequestOneDay> loadWeather(@Query("q") String cityCountry,
-                                     @Query("appid") String keyApi,
-                                     @Query("units") String units);
+                                           @Query("appid") String keyApi,
+                                           @Query("units") String units);
 
+    @GET("data/2.5/weather")
+    Call<WeatherRequestOneDay> loadWeatherLocation(@Query("lat") double lat,
+                                                   @Query("lon") double lon,
+                                                   @Query("appid") String keyApi,
+                                                   @Query("units") String units);
 }
