adc86b9f749f603029aff76f0bd2a6a60ad06616
silantyevmn <5992033@mail.ru>
2018-08-03 15:17:56 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/weather/Start.java
+++ app/src/main/java/silantyevmn/ru/weather/Start.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import silantyevmn.ru.weather.database.DataBaseSource;
 import silantyevmn.ru.weather.utils.CityPreference;
+import silantyevmn.ru.weather.utils.myLocation.MyLocation;
 
 /**
  * Created by silan on 04.07.2018.
@@ -15,7 +16,8 @@ public class Start extends Application {
     public void onCreate() {
         super.onCreate();
         //иницилизируем базу данных
-        DataBaseSource.initDataBase(this);
-        CityPreference.getPreference(this);
+        DataBaseSource.init(this);
+        CityPreference.init(this);
+        MyLocation.init();
     }
 }
